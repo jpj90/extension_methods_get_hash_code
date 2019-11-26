45197e59dd35d9859183f11faf7853c34551b88d
James Janse <james.janse@hypoport.com>
2019-11-26 15:23:55 +0100

    improved namespaces naming

--- ExtensionMethods/ExtensionMethods.cs
+++ ExtensionMethods/ExtensionMethods.cs
@@ -2,7 +2,7 @@
 using System.Collections.Generic;
 using System.Reflection;
 
-namespace ExtensionMethodsX
+namespace ExtensionMethods
 {
   public static class MyExtensions
   {
--- ExtensionMethods/Program.cs
+++ ExtensionMethods/Program.cs
@@ -1,9 +1,9 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Reflection;
-using ExtensionMethodsX;
+using ExtensionMethods;
 
-namespace ExtensionMethods
+namespace MyCode
 {
   class Program
   {
